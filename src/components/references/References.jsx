a892e73539b9fdb605c4a539cbd64439a3706a83
Fjodor Lundgren <fjodorlundgren@gmail.com>
2022-08-09 14:39:59 +0200

    reference change

--- src/components/references/References.jsx
+++ src/components/references/References.jsx
@@ -14,7 +14,7 @@ const References = () => {
            <div className="video">
           <iframe src="https://player.vimeo.com/video/726264674?h=d81040dd9c" height="360" width="640"   frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>          </div>
           <div className="video">
-          <iframe src="https://player.vimeo.com/video/726254645?h=275f12426b" height="360" width="640" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>          </div>
+          <iframe src="https://player.vimeo.com/video/737876774?h=1c063d071e" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>          </div>
           </div>
         <div className="full__references btn btn-primary">
           <a href="https://vimeo.com/user145814511" target="_blank">
